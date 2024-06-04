1a4584f162ec3f6515c756ff309f2f39f11ddc71
Michael Owen <mikalowen.g@gmail.com>
2024-06-04 11:33:51 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 
 public class Main {
   public static void main(String[] args) {
-    
+
+    // create game class
     GuessGame guessGame = new GuessGame();
 
+    // launch game
     guessGame.launch();
   }
 }
